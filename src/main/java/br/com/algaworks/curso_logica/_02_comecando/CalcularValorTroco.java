84277f907869cb393b126fc7168cc856cb17b813
Pedro <pedroferreiracjr@gmail.com>
2021-11-09 11:51:26 -0300

    criando o primeiro algoritmo - calculo de troco do produto

--- src/main/java/br/com/algaworks/curso_logica/_02_comecando/CalcularValorTroco.java
+++ src/main/java/br/com/algaworks/curso_logica/_02_comecando/CalcularValorTroco.java
@@ -1,7 +1,21 @@
 package br.com.algaworks.curso_logica._02_comecando;
 
+import java.util.Scanner;
+
 public class CalcularValorTroco {
 	public static void main(String[] args) {
-		System.out.println("Olá");
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite o valor do produto: ");
+		Double valorProduto = scanner.nextDouble();
+
+		System.out.print("Digite o valor do cliente: ");
+		Double valorCliente = scanner.nextDouble();
+
+		Double resultado = valorCliente - valorProduto;
+
+		System.out.println("O troco a ser dado ao cliente é: " + resultado);
+
+		scanner.close();
 	}
 }

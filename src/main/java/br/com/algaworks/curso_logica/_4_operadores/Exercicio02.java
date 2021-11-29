8dd61951eaf3f4ab7f9cc23de9151c54e0f077b6
Pedro <pedroferreiracjr@gmail.com>
2021-11-29 14:36:18 -0300

    resposta do exercicio

---
+++ src/main/java/br/com/algaworks/curso_logica/_4_operadores/Exercicio02.java
@@ -0,0 +1,18 @@
+package br.com.algaworks.curso_logica._4_operadores;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Double valorProduto = scanner.nextDouble();
+
+		Double valorFinalProduto = valorProduto;
+		if (valorProduto < 100.0) {
+			valorFinalProduto = valorProduto + 15.0d;
+		}
+
+		System.out.printf("O valor final do produto é: %.2f%n", valorFinalProduto);
+		scanner.close();
+	}
+}

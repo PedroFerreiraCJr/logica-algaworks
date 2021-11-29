f20f27867bbcf51338c3fd1a088f150cddcb3abb
Pedro <pedroferreiracjr@gmail.com>
2021-11-29 16:32:39 -0300

    aula sobre o operador de atribuição; e os operadores de atribuição compostos

---
+++ src/main/java/br/com/algaworks/curso_logica/_4_operadores/OperadoresAtribuicao.java
@@ -0,0 +1,25 @@
+package br.com.algaworks.curso_logica._4_operadores;
+
+public class OperadoresAtribuicao {
+	public static void main(String[] args) {
+		Double numero = 7d; // atribui o valor do lado direito do operador = para a variável
+
+//		numero = numero + 4;
+//		System.out.println("numero = numero + 4: " + numero);
+
+//		numero += 4;
+//		System.out.println("numero += 4: " + numero);
+
+//		numero -= 4;
+//		System.out.println("numero -= 4: " + numero);
+
+//		numero *= 4;
+//		System.out.println("numero *= 4: " + numero);
+
+//		numero /= 4;
+//		System.out.println("numero /= 4: " + numero);
+
+		numero %= 4;
+		System.out.println("numero %= 4: " + numero);
+	}
+}

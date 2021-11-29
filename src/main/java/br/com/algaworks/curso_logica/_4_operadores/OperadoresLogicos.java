4acaca4c89c09d8fdd4fe16a1380a4395ac5b227
Pedro <pedroferreiracjr@gmail.com>
2021-11-29 20:09:23 -0300

    aula sobre operadores lógicos

---
+++ src/main/java/br/com/algaworks/curso_logica/_4_operadores/OperadoresLogicos.java
@@ -0,0 +1,40 @@
+package br.com.algaworks.curso_logica._4_operadores;
+
+public class OperadoresLogicos {
+	public static void main(String[] args) {
+		Boolean carrinhoMaiorQue100 = true;
+		Boolean periodoPromocao = true;
+		Boolean jaFezCompraNaLoja = true;
+		Boolean pagamentoAVista = true;
+
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoPromocao;
+//		if (aplicarDesconto) {
+//			System.out.println("O desconto será aplicado");
+//		}
+//		else {
+//			System.out.println("O desconto não será aplicado na compra");
+//		}
+
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoPromocao;
+//		if (aplicarDesconto) {
+//			System.out.println("O desconto será aplicado");
+//		}
+//		else {
+//			System.out.println("O desconto não será aplicado na compra");
+//		}
+		
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoPromocao && jaFezCompraNaLoja
+		
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 || periodoPromocao || jaFezCompraNaLoja;
+		
+//		Boolean aplicarDesconto = carrinhoMaiorQue100 && periodoPromocao || jaFezCompraNaLoja;
+		
+		Boolean aplicarDesconto = carrinhoMaiorQue100 && (periodoPromocao || jaFezCompraNaLoja) && pagamentoAVista;
+		if (aplicarDesconto) {
+			System.out.println("O desconto será aplicado");
+		}
+		else {
+			System.out.println("O desconto não será aplicado na compra");
+		}
+	}
+}

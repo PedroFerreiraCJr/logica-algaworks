837938aa3d32688edaca9f12c9f75a193c17cdb2
Pedro <pedroferreiracjr@gmail.com>
2021-11-30 16:16:06 -0300

    aula sobre o operador de concatenação

---
+++ src/main/java/br/com/algaworks/curso_logica/_4_operadores/ConcatenacaoTexto.java
@@ -0,0 +1,14 @@
+package br.com.algaworks.curso_logica._4_operadores;
+
+public class ConcatenacaoTexto {
+	public static void main(String[] args) {
+		String inicio = "Esse é o ";
+		String meio = "º exemplo sobre ";
+		String fim = "concatenação de textos.";
+		Integer numeroExemplo = 1;
+
+		System.out.println(inicio + numeroExemplo + meio + fim);
+
+//		System.out.println("Esse " + meio + "de concatenação de textos");
+	}
+}

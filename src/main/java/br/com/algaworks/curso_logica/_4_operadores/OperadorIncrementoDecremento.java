a5655b1a3edebe4d3f1d56ef465c9d3ae06b8754
Pedro <pedroferreiracjr@gmail.com>
2021-11-30 16:26:34 -0300

    aula sobre operadores de incremento e decremento

---
+++ src/main/java/br/com/algaworks/curso_logica/_4_operadores/OperadorIncrementoDecremento.java
@@ -0,0 +1,35 @@
+package br.com.algaworks.curso_logica._4_operadores;
+
+public class OperadorIncrementoDecremento {
+	public static void main(String[] args) {
+//		Integer numero = 10;
+//		System.out.println("Número antes do incremento: " + numero);
+//		++numero;
+//		System.out.println("Número depois do incremento: " + numero);
+
+//		Neste caso, as duas variáveis conterão o mesmo valor, por conta do pré-incremento
+//		Integer numero = 10;
+//		Integer numero02 = ++numero;
+//		System.out.println("Número: " + numero + ", Número 02: " + numero02);
+		
+//		Neste caso, as variáveis terão valores diferentes, por conta do pós-incremento
+//		Integer numero = 10;
+//		Integer numero02 = numero++;
+//		System.out.println("Número: " + numero + ", Número 02: " + numero02);
+		
+//		Integer numero = 10;
+//		System.out.println("Número antes do incremento: " + numero);
+//		--numero;
+//		System.out.println("Número depois do incremento: " + numero);
+		
+//		Neste caso, as duas variáveis conterão o mesmo valor, por conta do pré-incremento
+//		Integer numero = 10;
+//		Integer numero02 = --numero;
+//		System.out.println("Número: " + numero + ", Número 02: " + numero02);
+		
+//		Neste caso, as variáveis terão valores diferentes, por conta do pós-incremento
+//		Integer numero = 10;
+//		Integer numero02 = numero--;
+//		System.out.println("Número: " + numero + ", Número 02: " + numero02);
+	}
+}

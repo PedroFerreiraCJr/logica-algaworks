529bbdc65e8d3aff46afa9e3a062cdf91d21bc3b
Pedro <pedroferreiracjr@gmail.com>
2021-12-06 11:49:32 -0300

    aula sobre a estrutura condicional if

---
+++ src/main/java/br/com/algaworks/curso_logica/_5_estruturas_de_decisao/EstruturaIf.java
@@ -0,0 +1,27 @@
+package br.com.algaworks.curso_logica._5_estruturas_de_decisao;
+
+public class EstruturaIf {
+	public static void main(String[] args) {
+		Double emprestimo = 4000d;
+		Double movimentacaoMedia = 2000d;
+		
+//		Boolean movimentaPeloMenosMetadeDoValor = (movimentacaoMedia * 2) >= emprestimo;
+		Boolean aContaTemTempoSuficienteDeAbertura = true;
+		Boolean temNomeLimpo = true;
+		
+//		Boolean liberarEmprestimo = movimentaPeloMenosMetadeDoValor
+//			&& aContaTemTempoSuficienteDeAbertura
+//			&& temNomeLimpo;
+
+		// É uma boa prática sempre facilitar a leitura da expressão criando variáveis com nomes descritivos
+		//ou até mesmo métodos para essa finalidade
+		if (((movimentacaoMedia * 2) >= emprestimo)
+				&& aContaTemTempoSuficienteDeAbertura
+				&& temNomeLimpo) {
+			System.out.println("Sim, é possível liberar o empréstimo");
+		}
+		else {
+			System.out.println("Não pode liberar o empréstimo");
+		}
+	}
+}

ce49b675cc2cd794aaef2d124165785b2aea9557
Pedro <pedroferreiracjr@gmail.com>
2021-12-18 13:52:55 -0300

    aula introdutória sobre métodos

---
+++ src/main/java/br/com/algaworks/curso_logica/_08_metodos/PrimeiroMetodo.java
@@ -0,0 +1,54 @@
+package br.com.algaworks.curso_logica._08_metodos;
+
+import java.util.Scanner;
+
+public class PrimeiroMetodo {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		imprimirTraco();
+
+		String[] cursos = new String[] { "Java EE", "Spring", "Java OO Avançado" };
+		System.out.println("Escolha um dos cursos abaixo: ");
+		for (int i = 0; i < cursos.length; i++) {
+			System.out.printf("[%d] %s%n", i, cursos[i]);
+		}
+
+		System.out.print("Qual é o curso que você deseja?: ");
+		Integer posicaoCursoEscolhido = scanner.nextInt();
+
+		Boolean posicaoCursoValida = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+		if (!posicaoCursoValida) {
+			System.err.println("Posição inválida!");
+			System.exit(1);
+		}
+
+		imprimirTraco();
+
+		String[] formasPagamento = new String[] { "Cartão", "Boleto" };
+		System.out.println("Escolha um das formas de pagamento abaixo: ");
+		for (int i = 0; i < formasPagamento.length; i++) {
+			System.out.printf("[%d] %s%n", i, formasPagamento[i]);
+		}
+		
+		System.out.print("Qual a forma de pagamento que você deseja?: ");
+		Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
+
+		Boolean posicaoFormaPagamentoValida = posicaoFormaPagamentoEscolhida >= 0 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+		if (!posicaoFormaPagamentoValida) {
+			System.err.println("Posição inválida!");
+			System.exit(1);
+		}
+		
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+		
+		imprimirTraco();
+		System.out.printf("O curso escolhido foi %s, e a forma de pagamento é %s%n", cursoEscolhido, formaPagamentoEscolhida);
+		scanner.close();
+	}
+	
+	static void imprimirTraco() {
+		System.out.println("---------------------------------------------------");
+	}
+}

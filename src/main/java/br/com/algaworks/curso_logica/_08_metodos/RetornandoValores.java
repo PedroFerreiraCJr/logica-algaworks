3e5f3de0c5ac4113b40f1e25bde4c56ca967b16d
Pedro <pedroferreiracjr@gmail.com>
2021-12-20 16:04:15 -0300

    aula sobre retornando valores em métodos

---
+++ src/main/java/br/com/algaworks/curso_logica/_08_metodos/RetornandoValores.java
@@ -0,0 +1,71 @@
+package br.com.algaworks.curso_logica._08_metodos;
+
+import java.util.Scanner;
+
+public class RetornandoValores {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		imprimirTraco();
+
+		String[] cursos = new String[] { "Java EE", "Spring", "Java OO Avançado" };
+		imprimir("Escolha um dos cursos abaixo: ");
+		iterarEExibirPosicoesDoVetorString(cursos);
+
+		Integer posicaoCursoEscolhido = prompt(scanner, "Qual é o curso que você deseja?: ");
+
+		Boolean posicaoCursoValida = posicaoInformadaEhValida(posicaoCursoEscolhido, cursos);
+		if (!posicaoCursoValida) {
+			encerrarProgramaPorCausaDaPosicaoInvalida();
+		}
+
+		imprimirTraco();
+
+		String[] formasPagamento = new String[] { "Cartão", "Boleto" };
+		imprimir("Escolha um das formas de pagamento abaixo: ");
+		iterarEExibirPosicoesDoVetorString(formasPagamento);
+
+		Integer posicaoFormaPagamentoEscolhida = prompt(scanner, "Qual a forma de pagamento que você deseja?: ");
+
+		Boolean posicaoFormaPagamentoValida = posicaoInformadaEhValida(posicaoFormaPagamentoEscolhida, formasPagamento);
+		if (!posicaoFormaPagamentoValida) {
+			encerrarProgramaPorCausaDaPosicaoInvalida();
+		}
+
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhida = formasPagamento[posicaoFormaPagamentoEscolhida];
+
+		imprimirTraco();
+		System.out.printf("O curso escolhido foi %s, e a forma de pagamento é %s%n", cursoEscolhido,
+				formaPagamentoEscolhida);
+		scanner.close();
+	}
+
+	static void imprimir(String texto) {
+		System.out.println(texto);
+	}
+	
+	static void imprimirTraco() {
+		System.out.println("---------------------------------------------------");
+	}
+
+	static int prompt(Scanner scanner, String mensagem) {
+		System.out.print(mensagem);
+		return scanner.nextInt();
+	}
+
+	static void encerrarProgramaPorCausaDaPosicaoInvalida() {
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}
+
+	static void iterarEExibirPosicoesDoVetorString(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("[%d] %s%n", i, array[i]);
+		}
+	}
+
+	static boolean posicaoInformadaEhValida(Integer posicao, String[] array) {
+		return posicao >= 0 && posicao < array.length;
+	}
+}

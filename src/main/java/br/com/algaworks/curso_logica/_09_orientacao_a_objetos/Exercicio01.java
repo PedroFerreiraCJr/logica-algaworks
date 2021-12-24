b405ab8086ee5e99c00b6f857efb3d68e815c165
Pedro <pedroferreiracjr@gmail.com>
2021-12-24 14:25:10 -0300

    resposta do exercicio 01 - orientação a objetos: validar necessidade de reposição de produto no estoque

---
+++ src/main/java/br/com/algaworks/curso_logica/_09_orientacao_a_objetos/Exercicio01.java
@@ -0,0 +1,22 @@
+package br.com.algaworks.curso_logica._09_orientacao_a_objetos;
+
+public class Exercicio01 {
+	public static void main(String[] args) {
+		Produto produto = new Produto();
+		produto.nome = "Smartphone";
+		produto.precoUnitario = 1000.0;
+		produto.quantidade = 20;
+		
+		boolean precisaRepor = produtoPrecisaSerRepostoNoEstoque(produto);
+		if (precisaRepor) {
+			System.out.printf("O produto %s precisa ser reposto no estoque.%n", produto.nome);
+		}
+		else {
+			System.out.printf("O produto %s não precisa ser reposto no estoque.%n", produto.nome);
+		}
+	}
+	
+	static boolean produtoPrecisaSerRepostoNoEstoque(Produto produto) {
+		return produto.quantidade <= 10;
+	}
+}
